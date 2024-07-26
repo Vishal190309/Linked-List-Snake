165578999516986019a82f5e71fee2a62a8dfc13
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-07-27 03:53:21 +0530

    Create BodyPart

---
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "UI/UIElement/ImageView.h"
+#include "Direction.h"
+namespace Player {
+	class BodyPart
+	{
+	protected:
+		UI::UIElement::ImageView* bodypart_image;
+
+		sf::Vector2i grid_position;
+		Direction direction;
+
+		float bodypart_width;
+		float bodypart_height;
+	};
+}
