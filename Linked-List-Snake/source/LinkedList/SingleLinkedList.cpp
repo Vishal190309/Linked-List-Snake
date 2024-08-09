0284d2c5c3da0bb19db04f391f7f3588abab7122
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-08-09 12:29:36 +0530

    Insert at End

--- Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
@@ -79,12 +79,13 @@ namespace LinkedList
 	}
 
 	void SingleLinkedList::insertNodeAtTail() {
+		linked_list_size++;
 		Node* new_node = createNode();
 		Node* cur_node = head_node;
 
 		if (cur_node == nullptr) {       // If the list is empty, set the new node as the head
 			head_node = new_node;
-			new_node->body_part.initialize(node_width, node_height, default_position, default_direction);
+			initializeNode(new_node, nullptr, Operation::TAIL);
 			return;
 		}
 
@@ -92,10 +93,10 @@ namespace LinkedList
 		while (cur_node->next != nullptr) {
 			cur_node = cur_node->next;
 		}
-
 		// Attach the new node at the end
 		cur_node->next = new_node;
-		new_node->body_part.initialize(node_width, node_height, getNewNodePosition(cur_node), cur_node->body_part.getDirection());
+		initializeNode(new_node, cur_node, Operation::TAIL);
+		
 	}
 
 	void SingleLinkedList::updateNodeDirection(Direction direction_to_set)
